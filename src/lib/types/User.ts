cfe7039e15113868d12071f2fd6649e239e6e0cf
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-08-04 15:31:20 -0600

    main commit

---
+++ src/lib/types/User.ts
@@ -0,0 +1,12 @@
+interface User {
+  name: string;
+  firstLastName: string;
+  secondLastName: string;
+  email: string;
+  password: string;
+  role: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+}
