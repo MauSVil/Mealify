497a40717abe01d479fe5db2ae8e545f42e5df3c
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-09-04 22:56:32 -0600

    Product repository

---
+++ src/lib/types/Zod/Product.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const ProductSchema = z.object({
+  _id: z.string().optional(),
+  name: z.string(),
+  description: z.string(),
+  price: z.number(),
+  available: z.boolean(),
+  image: z.string(),
+  createdAt: z.coerce.date().optional(),
+  updatedAt: z.coerce.date().optional(),
+  deletedAt: z.coerce.date().optional(),
+  restaurantId: z.string(),
+});
+
+export type Product = z.infer<typeof ProductSchema>;
+
+export const ProductRepositoryFilterModel = z.object({
+  name: z.string().optional(),
+  available: z.boolean().optional(),
+  restaurantId: z.string().optional(),
+});
+
+export type ProductRepositoryFilter = z.infer<typeof ProductRepositoryFilterModel>;
