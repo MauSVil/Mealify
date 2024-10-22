cb37c1e445785ff2fb0c55dff84fd89f95fd9c55
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-10-22 17:40:47 -0600

    ProductCard

--- src/app/user/(private)/businesses/[id]/_components/Content.tsx
+++ src/app/user/(private)/businesses/[id]/_components/Content.tsx
@@ -5,6 +5,7 @@ import { useProducts } from "../_hooks/useProducts";
 import Image from "next/image";
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
+import { ProductCard } from "./ProductCard";
 
 interface Props {
   id: string
@@ -15,17 +16,16 @@ const BusinessIdContent = (props: Props) => {
   const products = useMemo(() => productsQuery.data || [], [productsQuery.data]);
 
   return (
-    <div className="flex flex-1 flex-col gap-6 p-4 md:gap-12 md:p-8">
-      <div className="relative w-full h-32 md:h-64">
+    <div className="flex flex-col gap-12">
+      <div className="relative w-full h-16 md:h-36">
         <Image
           fill
           src="https://minio.mausvil.dev/businesses/67141c4d4373b7d9b4bb9064.jpeg"
           alt="Image"
           className="h-full w-full object-cover"
-          style={{ filter: "blur(3px)" }}
         />
         <Avatar
-          className="absolute -bottom-8 left-4 h-20 w-20 rounded-full"
+          className="absolute -bottom-8 left-4 h-20 w-20 rounded-full border-2"
         >
           <AvatarImage
             src="https://minio.mausvil.dev/businesses/67141c4d4373b7d9b4bb9064.jpeg"
@@ -34,13 +34,15 @@ const BusinessIdContent = (props: Props) => {
           />
         </Avatar>
       </div>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">
-          {products.length > 0 ? products[0].name : ""}
-        </h1>
-        <p className="text-sm text-muted-foreground">
-          {products.length > 0 ? products[0].description : ""}
-        </p>
+      <h1 className="text-2xl font-bold">
+        Chikabal
+      </h1>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+        {
+          products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        }
       </div>
     </div>
   )
--- src/app/user/(private)/businesses/[id]/page.tsx
+++ src/app/user/(private)/businesses/[id]/page.tsx
@@ -2,7 +2,7 @@ import BusinessIdContent from "./_components/Content";
 
 const UserBusinessPage = (props: { params: { id: string } }) => {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+    <div className="flex flex-1 flex-col gap-4 md:gap-8">
       <BusinessIdContent id={props.params.id} />
     </div>
   )
