e42251a3b810ab8d478daa226dbf719a71dcf8c9
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-10-22 17:51:59 -0600

    Business client name

--- src/app/user/(private)/businesses/[id]/_components/Content.tsx
+++ src/app/user/(private)/businesses/[id]/_components/Content.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { Avatar } from "@/components/ui/avatar";
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { ProductCard } from "./ProductCard";
+import { useClient } from "../_hooks/useClient";
+import { Business } from "@/lib/types/Zod/Business";
 
 interface Props {
   id: string
@@ -15,12 +17,15 @@ const BusinessIdContent = (props: Props) => {
   const productsQuery = useProducts(props.id);
   const products = useMemo(() => productsQuery.data || [], [productsQuery.data]);
 
+  const clientQuery = useClient(props.id);
+  const client = useMemo(() => clientQuery.data || {}, [clientQuery.data]) as Business;
+
   return (
     <div className="flex flex-col gap-12">
       <div className="relative w-full h-16 md:h-36">
         <Image
           fill
-          src="https://minio.mausvil.dev/businesses/67141c4d4373b7d9b4bb9064.jpeg"
+          src={client.heroImage as string}
           alt="Image"
           className="h-full w-full object-cover"
         />
@@ -28,14 +33,14 @@ const BusinessIdContent = (props: Props) => {
           className="absolute -bottom-8 left-4 h-20 w-20 rounded-full border-2"
         >
           <AvatarImage
-            src="https://minio.mausvil.dev/businesses/67141c4d4373b7d9b4bb9064.jpeg"
+            src={client.heroImage as string}
             alt="Avatar"
             className="h-full w-full object-cover"
           />
         </Avatar>
       </div>
       <h1 className="text-2xl font-bold">
-        Chikabal
+        {client.name}
       </h1>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
         {
---
+++ src/app/user/(private)/businesses/[id]/_hooks/useClient.ts
@@ -0,0 +1,21 @@
+import { Business } from "@/lib/types/Zod/Business";
+import ky from "ky";
+import { useQuery } from "react-query";
+import { toast } from "sonner";
+
+export const useClient = (businessId: string) => {
+  return useQuery({
+    queryKey: ["client", businessId],
+    queryFn: async () => {
+      const resp = await ky.post("/api/user/restaurants", { json: { id: businessId } }).json() as { data?: Business, error?: string };
+      return resp.data || {};
+    },
+    onError: (error) => {
+      if (error instanceof Error) {
+        toast.error(error.message);
+        return;
+      }
+      toast.error("Error al buscar productos");
+    },
+  })
+};
