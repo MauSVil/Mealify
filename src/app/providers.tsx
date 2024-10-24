eb6cdf1bb9bfbd02197500f1587e4e7448aa277b
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-10-23 22:56:09 -0600

    cart v1

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,29 +2,35 @@
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ReactNode } from "react";
+import { createContext, ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "sonner";
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { CartContextType } from "@/lib/types/Cart";
+import { CartProvider } from "./_providers/CartProvider";
+
+export const CartContext = createContext<CartContextType | undefined>(undefined);
 
 const Providers = ({ children }: { children: ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
-        <TooltipProvider>
-          <Toaster richColors />
-          {children}
-        </TooltipProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <CartProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+          <TooltipProvider>
+            <Toaster richColors />
+            {children}
+          </TooltipProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </CartProvider>
   );
 }
 
