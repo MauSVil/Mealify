41a71d2d6aece38e6ec21cb99e38a8a7b6f077e2
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-10-31 00:56:58 -0600

    small test?

--- src/app/api/user/orders/stripe-webhook/route.ts
+++ src/app/api/user/orders/stripe-webhook/route.ts
@@ -20,39 +20,51 @@ export async function POST(req: NextRequest) {
 
     const event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
 
-    if (event.type === 'checkout.session.completed') {
-      const session = event.data.object;
-      const sessionId = session.id;
-      const order = await OrderRepository.findOne({ checkoutSessionId: sessionId });
-      if (!order) {
-        return NextResponse.json({ error: 'No se encontró la orden' }, { status: 400 });
-      }
-      await OrderRepository.updateOne(order._id!, { status: 'paid' });
-
-      const shippingAmount = session?.metadata?.shippingAmount || 0;
-      const paymentIntentId = session.payment_intent;
-
-      const paymentIntent = await stripe.paymentIntents.retrieve(paymentIntentId as string);
+    // if (event.type === 'checkout.session.completed') {
+    //   const session = event.data.object;
+    //   const sessionId = session.id;
+  
+    //   const order = await OrderRepository.findOne({ checkoutSessionId: sessionId });
+    //   if (!order) {
+    //     return NextResponse.json({ error: 'No se encontró la orden' }, { status: 404 });
+    //   }
+    //   await OrderRepository.updateOne(order._id!, { status: 'paid' });
+    // }
+  
+    if (event.type === 'payment_intent.succeeded') {
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
       const chargeId = paymentIntent.latest_charge;
-
+  
       const charge = await stripe.charges.retrieve(chargeId as string);
       const balanceTransactionId = charge.balance_transaction;
-      const balanceTransaction = await stripe.balanceTransactions.retrieve(balanceTransactionId as string);
-
-      const netAmount = balanceTransaction.net;
-
-      console.log(netAmount, 'netAmount');
-
-      const platformFee = Math.round(netAmount * 0.10);
-      const deliveryFee = Math.round(netAmount * 0.05);
-      const restaurantAmount = netAmount - platformFee - deliveryFee;
-
-      console.log(platformFee, deliveryFee, restaurantAmount);
+  
+      if (balanceTransactionId) {
+        const balanceTransaction = await stripe.balanceTransactions.retrieve(balanceTransactionId as string);
+        const netAmount = balanceTransaction.net;
+  
+        console.log(`Monto neto después de comisiones: ${netAmount}`);
+  
+        const platformFee = Math.round(netAmount * 0.10);
+        const deliveryFee = Math.round(netAmount * 0.05);
+        const restaurantAmount = netAmount - platformFee - deliveryFee;
+  
+        console.log(`Comisión para plataforma: ${platformFee}`);
+        console.log(`Comisión para repartidor: ${deliveryFee}`);
+        console.log(`Monto para el restaurante: ${restaurantAmount}`);
+  
+      } else {
+        console.error('No se encontró el balance_transaction para el charge.');
+      }
     }
 
     return NextResponse.json({ received: true }, { status: 200 });
   } catch (err) {
-    console.error(`❌ Error message: ${err instanceof Error ? err.message : 'Unknown error'}`);
+    if (err instanceof Error) {
+      console.error(`❌ Error message: ${err.message}`);
+      return NextResponse.json({ error: err.message }, { status: 400 });
+    }
+    console.error(`❌ Error message: ${err}`);
     return NextResponse.json({ error: 'Webhook Error: Invalid signature' }, { status: 400 });
   }
 }
