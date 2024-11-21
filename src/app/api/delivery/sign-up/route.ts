95ffed3a5f95854ed34afeb0b857b2b0a75552dc
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-11-21 01:59:07 -0600

    delivery sign-up

--- src/app/api/delivery/sign-up/route.ts
+++ src/app/api/delivery/sign-up/route.ts
@@ -1,6 +1,9 @@
 import { UsersRepository } from "@/lib/Repositories/User.repository";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from 'bcrypt';
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 export const POST = async (req: NextRequest) => {
   try {
@@ -10,12 +13,12 @@ export const POST = async (req: NextRequest) => {
     }
     const user = await UsersRepository.findOne({ email, role: 'delivery' });
     if (user) {
-      return NextResponse.json({ error: 'User already exists' }, { status: 400 });
+      return NextResponse.json({ error: 'Delivery already exists' }, { status: 400 });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    await UsersRepository.insertOne({
+    const insertedId = await UsersRepository.insertOne({
       email,
       password: hashedPassword,
       role: 'delivery',
@@ -24,12 +27,26 @@ export const POST = async (req: NextRequest) => {
       stripeAccountId: null,
       onboardingFinished: false,
     });
-    return NextResponse.json({ message: 'Repartidor registrado correctamente' });
+
+    const insertedUser = await UsersRepository.findOne({ id: insertedId });
+
+    if (!insertedUser) throw new Error('No se ha podido crear el usuario');
+
+    const account = await stripe.accounts.create({
+      type: "express",
+      email: email,
+      business_type: "individual",
+      country: "MX",
+    });
+
+    await UsersRepository.updateOne(insertedUser._id!, { stripeAccountId: account.id });
+
+    return NextResponse.json({ data: 'Se creo el repartidor correctamente' });
   } catch (e) {
     console.error('Error signing up', e);
     if (e instanceof Error) {
       return NextResponse.json({ error: e.message }, { status: 400 });
     }
-    return NextResponse.json({ error: 'Error [SignupDelivery]' }, { status: 400 });
+    return NextResponse.json({ error: 'Error [SignupAdmin]' }, { status: 400 });
   }
 };
--- src/app/delivery/(public)/sign-up/_hooks/useSignUp.ts
+++ src/app/delivery/(public)/sign-up/_hooks/useSignUp.ts
@@ -1,21 +1,21 @@
-import { UserSignUp } from "@/lib/types/Zod/UserSignUp";
+import { AdminSignUp } from "@/lib/types/Zod/AdminSignUp";
 import ky from "ky";
-import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export const useSignUp = () => {
-  const router = useRouter();
+  const router = useRouter()
   return useMutation({
-    mutationFn: async (values: UserSignUp) => {
-      const resp = await ky.post("/api/delivery/sign-up", { json: values }).json() as { message?: string, error?: string };
+    mutationFn: async (values: AdminSignUp) => {
+      const resp = await ky.post("/api/delivery/sign-up", { json: values }).json() as { data?: string, error?: string };
       return resp;
     },
     onSuccess: (resp) => {
-      toast.success(resp.message)
+      toast.success(resp.data)
       setTimeout(() => {
         router.push("/delivery/onboard")
-      }, 1000)
+      }, 1000);
     },
     onError: () => {
       toast.error("Error al registrarse")
