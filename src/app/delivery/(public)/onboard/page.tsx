50bed131a17f746421cf9a6e3169f0fc78d3247c
Mauricio Sanchez Vilchis <mausvilchis@Mauricios-MacBook-Pro.local>
2024-11-23 03:09:08 -0600

    formity working

--- src/app/delivery/(public)/onboard/page.tsx
+++ src/app/delivery/(public)/onboard/page.tsx
@@ -1,9 +1,28 @@
-const DeliveryOnboard = () => {
-  return (
-    <div>
-      <h1>Onboarding</h1>
-    </div>
-  )
-}
+"use client";
 
-export default DeliveryOnboard;
+import type { Value } from "expry";
+
+import { AnimatePresence, motion } from "framer-motion";
+import Form from "@/components/DeliveryOnboard/Form";
+
+
+export default function Home() {
+  function handleReturn(result: Value) {
+    console.log(result);
+  }
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        key="form"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className="h-full"
+      >
+        <Form onReturn={handleReturn} />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
