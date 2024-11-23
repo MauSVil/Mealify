6b93827f8d14c0683508bcc0632d97da456c2d5e
Mau S Vilchis <maudev96@gmail.com>
2024-11-23 14:41:37 -0600

    radio group

--- src/components/formity/fields/radio-group.tsx
+++ src/components/formity/fields/radio-group.tsx
@@ -1,11 +1,8 @@
 import { useId } from "react";
-import { Radio, RadioGroup as HeadlessRadioGroup } from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/20/solid";
-
+import { RadioGroup as RadioGroupS, RadioGroupItem } from "@/components/ui/radio-group";
 
 import Field from "../user-interface/field";
-import Input from "../user-interface/input";
-import { cn } from "@/lib/utils";
+import { Label } from "@/components/ui/label";
 
 interface RadioGroupProps {
   label: string;
@@ -20,28 +17,20 @@ export default function RadioGroup({ label, value, onChange, options, direction,
   const id = useId();
   return (
     <Field id={id} label={label} error={error}>
-      <HeadlessRadioGroup
-        value={value}
-        onChange={onChange}
-        className={cn("peer grid grid-cols-1 gap-4", {
-          "grid-cols-[repeat(auto-fit,minmax(theme(spacing.40),1fr))]": direction === "x",
-        })}
+      <RadioGroupS
+        defaultValue={value}
+        onValueChange={onChange}
       >
         {options.map((option) => (
-          <Input
-            key={option.value}
-            as={Radio}
-            props={{ value: option.value }}
-            className={cn(
-              "group flex cursor-pointer items-center gap-2 focus:outline-none data-[checked]:border-neutral-500",
-              { "border-red-500 data-[checked]:border-red-500": error },
-            )}
-          >
-            {option.label}
-            <CheckIcon className="pointer-events-none ml-auto size-5 fill-white/50 group-data-[checked]:fill-white/100" />
-          </Input>
+          <div className="flex items-center gap-2" key={option.value}>
+            <RadioGroupItem
+              value={option.value}
+            />
+            <Label>{option.label}</Label>
+          </div>
         ))}
-      </HeadlessRadioGroup>
+      </RadioGroupS>
+      {error && <p className="text-sm text-red-500">{error.message}</p>}
     </Field>
   );
 }
